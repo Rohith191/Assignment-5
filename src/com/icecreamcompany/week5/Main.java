9a1db060bfbe433fa4c7bac88c066efa6395d16d
rohith191 <rohiththota@lewisu.edu>
2021-10-01 12:51:08 -0500

    prettified output

--- src/com/icecreamcompany/week5/Main.java
+++ src/com/icecreamcompany/week5/Main.java
@@ -4,7 +4,9 @@ package com.icecreamcompany.week5;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("============================================");
 		System.out.println("Happy path forward only usecase:");
+		System.out.println("============================================");
 		var order = new IceCreamOrderProcess();
 		order.startNewIceCreamOrderProcess();
 		order.printStep();
@@ -15,8 +17,9 @@ public class Main {
 		order.proceedToNextStep();
 		order.printStep();
 		order.proceedToNextStep();
-		
+		System.out.println("============================================");
 		System.out.println("Forward with backward step usecase:");
+		System.out.println("============================================");
 		var order1 = new IceCreamOrderProcess();
 		order1.startNewIceCreamOrderProcess();
 		order1.printStep();
@@ -31,8 +34,9 @@ public class Main {
 		order1.proceedToNextStep();
 		order1.printStep();
 		order1.proceedToNextStep();
-		
+		System.out.println("============================================");
 		System.out.println("Forward with order canceled step usecase:");
+		System.out.println("============================================");
 		var order2 = new IceCreamOrderProcess();
 		order2.startNewIceCreamOrderProcess();
 		order2.printStep();
